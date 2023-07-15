e94eb49690d8b2a2e64289eedf444b5aa89a82f7
afonyya <afonyaafona@gmail.com>
2023-07-15 16:56:38 +0300

    base controller

---
+++ src/common/route.interface.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction, Router } from 'express';
+
+export interface IControllerRoute {
+  path: string;
+  method: keyof Pick<Router, 'get' | 'post' | 'put' | 'patch' | 'delete'>;
+  handler: (req: Request, res: Response, next: NextFunction) => void;
+}
