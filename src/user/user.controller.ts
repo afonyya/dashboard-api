6d3ceedbcd5d2829d07b774fbe86441ca82a2705
afonyya <afonyaafona@gmail.com>
2023-07-22 00:33:37 +0300

    add jwt token auth

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,12 +10,15 @@ import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
 import { IUserService } from './user.service.interface';
 import { ValidateMiddleware } from '../common/validate.middleware';
+import { sign } from 'jsonwebtoken';
+import { IConfigService } from '../config/config.service.interface';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
   constructor(
     @inject(TYPES.ILogger) private loggerService: ILogger,
     @inject(TYPES.IUserService) private userService: IUserService,
+    @inject(TYPES.IConfigService) private configService: IConfigService,
   ) {
     super(loggerService);
     this.bindRoutes([
@@ -41,7 +44,7 @@ export class UserController extends BaseController implements IUserController {
   ): Promise<void> {
     const result = await this.userService.createUser(body);
     if (!result) {
-      next(new HTTPError(422, 'user already exist'));
+      next(new HTTPError(422, this.configService.get('SECRET')));
     } else {
       this.ok(res, { email: result.email, id: result.id });
     }
@@ -56,7 +59,24 @@ export class UserController extends BaseController implements IUserController {
     if (!result) {
       next(new HTTPError(401, 'auth error', 'login'));
     } else {
-      this.ok(res, {});
+      const token = await this.signToken(body.email, 'secret');
+      this.ok(res, { token });
     }
+  }
+
+  signToken(email: string, secret: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      sign(
+        { email, iat: Math.floor(Date.now() / 1000) },
+        secret,
+        { algorithm: 'HS256' },
+        (err, token) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(token as string);
+        },
+      );
+    });
   }
 }
